04fbc097f094c69d01224d4f624bcf79fa3c0ab0
Martin Slota <msl@issuu.com>
2019-08-05 11:30:41 +0200

    Applied ocamlformat

--- src/test/test.ml
+++ src/test/test.ml
@@ -1,9 +1,8 @@
 module Basic = Basic.Protoc.Plugin.Test
-
 module Simple = Simple.Outer_package.Inner_package
 
 let () =
   let payload = 42 in
   let _basic_message : Basic.Message.t = {payload} in
-  let _simple_message : Simple.C.t = { type_1 = payload; type_ = None; type' = None} in
+  let _simple_message : Simple.C.t = {type_1 = payload; type_ = None; type' = None} in
   ()
