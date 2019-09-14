606ac8ff6c27e9993b5f9be882965e5d198ccd95
Anders Fugmann <anders@fugmann.net>
2019-09-14 23:58:36 +0200

    Fix expected result

--- test/enum_test.ml
+++ test/enum_test.ml
@@ -8,4 +8,4 @@ let%expect_test _ =
   let module T = Enum.Outside in
   let t = T.{ enum = Enum.E1.C } in
   Test_lib.test_encode "enum.proto" (module T) t;
-  [%expect {| enum: B; |}]
+  [%expect {| enum: C; |}]
