0eef1b7f595f2d0f4aba8a13ce9a40763ba701ae
Anders Fugmann <anders@fugmann.net>
2019-09-25 23:18:24 +0200

    Fix mutual recursive modules

--- test/recursive_test.ml
+++ test/recursive_test.ml
@@ -3,3 +3,11 @@ let%expect_test _ =
   let t = T.{ m = Some { m = Some { m = None } }} in
   Test_lib.test_encode (module T) t;
   [%expect {| m {;  m {;  }; }; |}]
+
+
+let%expect_test _ =
+  let module T1 = Recursive.Mutual1 in
+  let module T2 = Recursive.Mutual2 in
+  let t = T1.{ m2 = Some T2.{ m1 = Some T1.{ m2 = Some T2.{ m1 = None }}}} in
+  Test_lib.test_encode (module T1) t;
+  [%expect {| m2 {;  m1 {;  m2 {;  };  }; }; |}]
