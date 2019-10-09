a41cd813cd8cad29df7114c4417f9196e28f6ff9
Wojtek Czekalski <me@wczekalski.com>
2019-10-09 11:43:23 +0200

    Change Protobuf.Infix.Int64.lsr to use shift_right_logical
    Arithmetical shift_right sets the sign bit

--- src/protobuf/Infix.ml
+++ src/protobuf/Infix.ml
@@ -2,7 +2,7 @@ module Int64 = struct
   open Int64
   let (land) = logand
   let (lsl) = shift_left
-  let (lsr) = shift_right
+  let (lsr) = shift_right_logical
   let (lor) = logor
   let (lxor) = logxor
   let (+) = add
