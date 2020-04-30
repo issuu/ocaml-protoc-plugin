72794f64bc34c63b69de22e5d3478d0af60ec86a
Anders Fugmann <af@issuu.com>
2020-04-30 21:19:00 +0200

    Move userdefined opens before inported modules, to allow override of default google types

--- src/plugin/emit.ml
+++ src/plugin/emit.ml
@@ -254,6 +254,7 @@ let parse_proto_file ~params scope
   Code.emit implementation `None "";
   Code.emit implementation `None "open Ocaml_protoc_plugin.Runtime [@@warning \"-33\"]";
   Code.emit implementation `None "open Ocaml_protoc_plugin.Google_types [@@warning \"-33\"]";
+  List.iter ~f:(Code.emit implementation `None "open %s") params.opens;
   let _ = match dependencies with
     | [] -> ()
     | dependencies ->
@@ -266,8 +267,6 @@ let parse_proto_file ~params scope
       Code.emit implementation `End "end";
       Code.emit implementation `None "(**/**)";
   in
-  List.iter ~f:(Code.emit implementation `None "open %s") params.opens;
-
   wrap_packages ~params ~syntax scope message_type services (Option.value_map ~default:[] ~f:(String.split_on_char ~sep:'.') package)
   |> Code.append implementation;
 
