70f1e2d747dc3b7f302fe45159921bbd2a003cff
Et7f3 <cadeaudeelie@gmail.com>
2020-07-01 16:04:56 +0200

    Convert bash to OCaml.

---
+++ src/gen/gen.ml
@@ -0,0 +1,18 @@
+let _ = Sys.getenv "INCLUDE_GOOGLE_PROTOBUF"
+
+let () =
+    match Sys.getenv_opt "INCLUDE_GOOGLE_PROTOBUF" with
+      Some s -> print_endline s
+    | None ->
+            let suffix = "google/protobuf" in
+            let prefix = "/usr/include" in
+            let name = Filename.concat prefix suffix in
+            if Sys.file_exists name && Sys.is_directory name then
+                print_endline name
+            else
+                let prefix = "/usr/local/include" in
+                let name = Filename.concat prefix suffix in
+                if Sys.file_exists name && Sys.is_directory name then
+                    print_endline name
+                else
+                    failwith "Where is the include folder ?"
