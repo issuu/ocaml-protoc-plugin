90660c39e19fad07107cb66729754676d8ad7621
Wojtek Czekalski <me@wczekalski.com>
2022-08-04 10:38:09 +0200

    Add support for M1 homebrew  /opt/homebrew/include
    /opt/homebrew/include is the new include path

--- src/gen/gen.ml
+++ src/gen/gen.ml
@@ -3,14 +3,15 @@ let () =
       Some s -> print_endline s
     | None ->
             let suffix = "google/protobuf" in
-            let prefix = "/usr/include" in
-            let name = Filename.concat prefix suffix in
-            if Sys.file_exists name && Sys.is_directory name then
-                print_endline prefix
-            else
-                let prefix = "/usr/local/include" in
-                let name = Filename.concat prefix suffix in
-                if Sys.file_exists name && Sys.is_directory name then
-                    print_endline prefix
-                else
-                    failwith "Where is the include folder ?"
+            let check_dir prefix =
+              let name = Filename.concat prefix suffix in
+              if Sys.file_exists name && Sys.is_directory name then
+                print_endline prefix;
+                true
+              else
+                false
+            in
+            if not check_dir "/usr/include" then
+              if not check_dir "/usr/local/include" then
+                if not check_dir "/opt/homebrew/include" then
+                  failwith "Where is the include folder ?"
