5de45b821d52ca4c6ad895b1aebe9437fe86bfa0
Wojtek Czekalski <me@wczekalski.com>
2022-08-04 10:46:24 +0200

    Syntax error fixes

--- src/gen/gen.ml
+++ src/gen/gen.ml
@@ -5,13 +5,13 @@ let () =
             let suffix = "google/protobuf" in
             let check_dir prefix =
               let name = Filename.concat prefix suffix in
-              if Sys.file_exists name && Sys.is_directory name then
+              if Sys.file_exists name && Sys.is_directory name then (
                 print_endline prefix;
-                true
-              else
-                false
+                true)
+              else false
             in
-            if not check_dir "/usr/include" then
-              if not check_dir "/usr/local/include" then
-                if not check_dir "/opt/homebrew/include" then
-                  failwith "Where is the include folder ?"
+            if
+              not
+                (List.exists check_dir
+                   [ "/usr/include"; "/usr/local/include"; "/opt/homebrew/include" ])
+            then failwith "Where is the include folder ?"
