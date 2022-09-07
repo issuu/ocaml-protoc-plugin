01eff90c7363a31c01b5dfca1e4d310805ebeb72
Andreas Dahl <ad@issuu.com>
2022-09-07 10:00:02 +0200

    Add how response to echo test

--- examples/echo/test.ml
+++ examples/echo/test.ml
@@ -11,7 +11,7 @@ let mk_timestamp () =
 let mk_request () =
   Echo.Request.{ timestamp = `Ts (mk_timestamp ()); what = `Type Echo.Request.Who.World; _how = `not_set }
 
-let mk_reply Echo.Request.{ timestamp; what } =
+let mk_reply Echo.Request.{ timestamp; what; _how } =
   let at =
     match timestamp with
       | `Ts {seconds; nanos = _} ->
@@ -22,11 +22,12 @@ let mk_reply Echo.Request.{ timestamp; what } =
           "whenever"
   in
 
-  match what with
-    | `Someone person -> Printf.sprintf "%s Hello there, %s" at person
-    | `Type Echo.Request.Who.Mum -> Printf.sprintf "%s Hi Mom" at
-    | `Type Echo.Request.Who.World -> Printf.sprintf "%s Hello World" at
-    | `not_set -> Printf.sprintf "Hello Unknown"
+  match _how, what with
+    | `How how, _ -> Printf.sprintf "%s is how" how
+    | `not_set, `Someone person -> Printf.sprintf "%s Hello there, %s" at person
+    | `not_set,`Type Echo.Request.Who.Mum -> Printf.sprintf "%s Hi Mom" at
+    | `not_set,`Type Echo.Request.Who.World -> Printf.sprintf "%s Hello World" at
+    | `not_set,`not_set -> Printf.sprintf "Hello Unknown"
 
 let handle_request proto_request =
   let (decode, encode) = Ocaml_protoc_plugin.Service.make_service_functions Echo.Echo.call in
