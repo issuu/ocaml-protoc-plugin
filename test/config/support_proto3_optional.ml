9a3b3d20ec0a8823fbbf39a242b43482baec1b66
Anders Fugmann <anders@fugmann.net>
2022-09-10 15:55:57 +0200

    Dont use dune-configurator for protoc compatibility testing

--- test/config/support_proto3_optional.ml
+++
@@ -1,7 +0,0 @@
-module C = Configurator.V1
-
-let () =
-  C.main ~name:"support_proto3_optionals" (fun t ->
-    let r = C.Process.run t "protoc" ["--experimental_allow_proto3_optional"; "--version"] in
-    Printf.printf "%b" (r.exit_code = 0)
-  )
