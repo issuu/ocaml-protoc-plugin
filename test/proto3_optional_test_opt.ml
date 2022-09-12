5fd6b37e8f720e93bdf0cc8435c64d4cc8e237fb
Anders Fugmann <anders@fugmann.net>
2022-09-12 15:07:49 +0200

    Update expected result for proto3 optional fields

--- test/proto3_optional_test_opt.ml
+++ test/proto3_optional_test_opt.ml
@@ -4,10 +4,12 @@ let%expect_test _ =
   let module T = Proto3_optional.Message in
   let t = T.make ~payload:5 () in
   Test_lib.test_encode ~protoc_args:["--experimental_allow_proto3_optional"] (module T) t;
-  [%expect {| (Some 5) |}]
+  [%expect {| payload: 5 |}]
 
 let%expect_test _ =
   let module T = Proto3_optional.Message2 in
   let t = T.make ~payload:5 ~payload3:7 () in
   Test_lib.test_encode ~protoc_args:["--experimental_allow_proto3_optional"] (module T) t;
-  [%expect {| (Some 5) |}]
+  [%expect {|
+    payload: 5
+    payload3: 7 |}]
