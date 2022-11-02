3bc4ecc571322fba8fc58014ac49e0078e2087bc
Nymphium <s1311350@gmail.com>
2022-11-02 14:57:11 +0900

    Update src/ocaml_protoc_plugin/service.ml
    Co-authored-by: Anders Peter Fugmann <andersfugmann@github.com>

--- src/ocaml_protoc_plugin/service.ml
+++ src/ocaml_protoc_plugin/service.ml
@@ -15,10 +15,6 @@ let make_client_functions (type req) (type rep)
      (module Response : Message with type t = rep)) =
   Request.to_proto, Response.from_proto
 
-let make_client_functions' (type req) (type rep)
-    (module R : Rpc with type Request.t = req and type Response.t = rep) =
-  make_client_functions ((module R.Request), (module R.Response))
-
 let make_service_functions (type req) (type rep)
     ((module Request : Message with type t = req),
     (module Response : Message with type t = rep)) =
