2db12b6a0628cc1eb8842d25918d837ba9ba4430
Anders Fugmann <anders@fugmann.net>
2023-12-30 01:51:42 +0100

    Add test for name mangling

---
+++ test/mangle_names_test.ml
@@ -0,0 +1,7 @@
+module _ = Mangle_names.Camel_case_name
+module _ = Mangle_names.Camel_case_name'
+module _ = Mangle_names.Request_one
+module _ = Mangle_names.Response_one
+module _ = Mangle_names.String_of_int.Call_one
+module _ = Mangle_names.String_of_int.Call_one'
+module _ = Mangle_names.String_of_int.Call_one''
